120c39219e45d2ca34eb0bd128a929ab32ce14a2
Jessi Shank <shankj3@uw.edu>
2017-02-12 20:18:41 -0800

    pulling out static variables

---
+++ VARS.py
@@ -0,0 +1,33 @@
+import datetime
+import os
+
+from pygal.style import Style
+
+
+API_KEY = 'AIzaSyCtLpZ3MKo33ziOynkUbDJwqvF_baYY1ls'
+GOOGLE_CIVIC_KEY = 'AIzaSyDny6NNitDS3FIGkXWKO8sMgsNb9-G-h6E'
+OS_API_KEY= 'f8686fdf6e4871299219f398f88d508a'
+OPEN_FEC_KEY = 'FiiYWSsRi01pGXUNkfhwbEX6tF84AJpJq2zp3gzq'
+
+GOOGLE_GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json'
+GOOGLE_CIVIC_ENDPOINT = 'https://www.googleapis.com/civicinfo/v2/representatives'
+LEGISLATOR_ENDPOINT = 'http://openstates.org/api/v1/legislators/geo/'
+BILL_ENDPOINT = 'http://openstates.org/api/v1/bills/'
+COMMITTEE_ENDPOINT = 'http://openstates.org/api/v1/committees/{0}'
+OPEN_FEC_ENDPOINT = 'https://api.open.fec.gov/v1'
+
+CURRENT_YEAR = datetime.datetime.now().year
+
+CUSTOM_FONT = os.path.join('static', 'Helvetica-Light.ttf')
+
+
+SOCIAL_ENDPOINTS = {
+    'Facebook' : 'https://www.facebook/com/',
+    'Twitter' : 'https://twitter.com/',
+    'YouTube' : 'https://www.youtube.com/user/',
+    'GooglePlus' : 'https://plus.google.com/'
+}
+
+LEGIS_STYLE = Style(background='transparent',
+                    plot_background='transparent',
+                    transition='400ms ease-in')
