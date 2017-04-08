152ab0f00f7ec563d0c976626c7cdb29f7cfd3f9
Marianne Feng <feng.marianne@gmail.com>
2017-04-08 08:47:57 -0700

    removing unused comment

--- legis_data/process/legwork.py
+++ legis_data/process/legwork.py
@@ -465,26 +465,24 @@ def get_upcoming_bills(valid_time_frame):
     root = upcoming_bills.getroot()
     this_week = []
 
-    # make this logic better here - can make use of xmltree's find and whatnot
-    for child in root:
-        if child.tag == 'category':
-            for floor_items in child:
-                for floor_item in floor_items:
-                    bill = {
-                        'type': child.attrib['type'],
-                        'floor_item': {
-                            'id': None,
-                            'title': None,
-                            'floor_text': None,
-                            'files': []
-                        }
-                    }
-                    bill['floor_item']['id'] = floor_item.attrib['id']
-                    bill['floor_item']['title'] = floor_item.find('legis-num').text
-                    bill['floor_item']['floor_text'] = floor_item.find('floor-text').text
-                    for file in floor_item.find('files'):
-                        bill['floor_item']['files'].append(file.attrib['doc-url'])
-                    this_week.append(bill)
+    category = root.find('category')
+    for floor_items in category:
+        for floor_item in floor_items:
+            bill = {
+                'type': category.attrib['type'],
+                'floor_item': {
+                    'id': None,
+                    'title': None,
+                    'floor_text': None,
+                    'files': []
+                }
+            }
+            bill['floor_item']['id'] = floor_item.attrib['id']
+            bill['floor_item']['title'] = floor_item.find('legis-num').text
+            bill['floor_item']['floor_text'] = floor_item.find('floor-text').text
+            for file in floor_item.find('files'):
+                bill['floor_item']['files'].append(file.attrib['doc-url'])
+            this_week.append(bill)
     return this_week
 
 
